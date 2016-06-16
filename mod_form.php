49ec13d7043668ec7eaa62e10a3c3d587d8c736e
Juan Pablo de Castro <juan.pablo.de.castro@gmail.com>
2016-06-16 18:23:14 +0200

    Compatibility with Moodle 2.6

--- mod_form.php
+++ mod_form.php
@@ -66,7 +66,7 @@ class mod_quest_mod_form extends moodleform_mod {
 		$mform->setType('name', PARAM_TEXT);
 		$mform->addRule('name', null, 'required', null, 'client'); //??
     // Adding the description fields.
-         $this->standard_intro_elements(get_string('description', 'quest'));
+        $this->standard_intro_elements(get_string('description', 'quest'));
         $mform->addElement('filemanager', 'introattachments',
                             get_string('introattachments', 'quest'),
                             null, array('subdirs' => 0, 'maxbytes' => $COURSE->maxbytes) );
